5df5d579f5434f5fe2e869cbec59f5890e8cdf0b
abdullahehab <30800958+abdullahehab@users.noreply.github.com>
2018-01-20 14:25:53 +0200

    layout adminPanel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,10 +25,10 @@
 <body>
     <div id="app">
         <div class="header">
-            <div class="container"> <a class="navbar-brand" href="index.html"><i class="fa fa-paper-plane"></i> ONE</a>
-                <div class="menu"> <a class="toggleMenu" href="#"><img src="images/nav_icon.png" alt="" /> </a>
+            <div class="container"> <a class="navbar-brand" href="{{ url('/') }}"><i class="fa fa-paper-plane"></i>ONE</a>
+                <div class="menu"> <a class="toggleMenu" href="#"><img src="{{ Request::root() }}/website/images/nav_icon.png" alt="" /> </a>
                     <ul class="nav" id="nav">
-                        <li class="current"><a href="index.html">Home</a></li>
+                        <li class="current"><a href="{{ url('/home') }}">Home</a></li>
                         <li><a href="about.html">About Us</a></li>
                         <li><a href="services.html">Services</a></li>
                         <li><a href="contact.html">Contact Us</a></li>
